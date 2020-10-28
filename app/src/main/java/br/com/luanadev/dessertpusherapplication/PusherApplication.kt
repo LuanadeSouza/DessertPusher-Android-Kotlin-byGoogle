a02a2cb89c04ac0fa39e61208e63da5a37bacc1c
Luana Souza <luana.souza@ifood.com.br>
2020-10-28 09:43:43 -0300

    using log to learn or lifecycle of an application

---
+++ app/src/main/java/br/com/luanadev/dessertpusherapplication/PusherApplication.kt
@@ -0,0 +1,13 @@
+package br.com.luanadev.dessertpusherapplication
+
+import android.app.Application
+import timber.log.Timber
+
+
+class PusherApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
